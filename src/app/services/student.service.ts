a8ac35c537ab2818f668a38ccd3f668a6e446bda
IamSumitJangid <sumitjangid471@gmail.com>
2019-01-29 19:49:10 +0530

    student fetch from params id

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { Student } from '../shared/student';
 
 const STUDENTS: Student[] = [
-  { id: 1, name: 'b', mobile_number: "+91-XXXXXXXXXX" },
-  { id: 2, name: 'a', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 1, name: 'a', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 2, name: 'b', mobile_number: "+91-XXXXXXXXXX" },
   { id: 3, name: 'c', mobile_number: "+91-XXXXXXXXXX" },
-  { id: 4, name: 'd', mobile_number: "+91-XXXXXXXXXX" }
+  { id: 4, name: 'd', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 5, name: 'e', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 6, name: 'f', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 7, name: 'g', mobile_number: "+91-XXXXXXXXXX" },
+  { id: 8, name: 'h', mobile_number: "+91-XXXXXXXXXX" }
 ];
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +22,10 @@ export class StudentService {
 
   getStudents(): Student[] {
     return STUDENTS;
+  }
+
+  getStudent(id: number): Student {
+    return STUDENTS.filter((student) => (student.id === id))[0];
   }
 
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { StudentService } from '../services/student.service';
+import { Student } from '../shared/student';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent implements OnInit {
-
-  constructor() { }
+export class StudentComponent implements OnInit, OnDestroy {
+  student: Student;
+  // subscriber: any;
+  constructor(private studentService: StudentService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    // this.subscriber = this.route.params.subscribe(params => {
+    //   this.id = +params['id']; // (+) converts string 'id' to a number
+    // });
+    let id = +this.route.snapshot.params['id'];
+    this.student = this.studentService.getStudent(id);
+  }
+
+  ngOnDestroy() {
+    // this.subscriber.unsubscribe();
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
